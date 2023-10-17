ef9c912db5bd04bfd27d27db041e6d157f6ea6c1
Gabriel Damian <gabbydamian92@gmail.com>
2023-10-17 08:40:58 +0100

    Decryption of cipher works regardless of randomness of cipher

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ const key = 'apple';
 
 function encrypt(text, key, type, iv) {
   const options = { iv: iv };
-  const cipher = CryptoJS[type].encrypt(text, key).toString();
+  const cipher = CryptoJS[type].encrypt(text, key, options).toString();
   return cipher;
 }
 
 function decrypt(text, key, type, iv) {
   const options = { iv: iv };
   const decipher = CryptoJS[type]
-    .decrypt(text, key)
+    .decrypt(text, key, options)
     .toString(CryptoJS.enc.Utf8);
   return decipher;
 }
 
 const iv = CryptoJS.enc.Hex.parse('101112131415161718191a1b1c1d1e1f');
 
-let txt = encrypt('Hello World', key, encryption_type[0], iv);
-console.log(txt);
-let txt2 = decrypt(txt, key, encryption_type[0], iv);
+let txt = encrypt('Hello not World', key, encryption_type[0], iv);
+let txt2 = decrypt(
+  txt,
+  key,
+  encryption_type[0],
+  iv
+);
 
 console.log(txt2);
-console.log(iv.toString());
+console.log(txt);
+
