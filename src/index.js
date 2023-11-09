8bc49a3c1fe55d53f7cdb2b5f6e42983136316d7
Damian Gabriel <gabbydamian92@gmail.com>
2023-11-09 16:10:19 +0100

    Fixed text wrapping in output field, added notify library and bootstrap icons

--- src/index.js
+++ src/index.js
@@ -3,34 +3,39 @@ const encBtn = document.getElementById("encBtn");
 const encKey = document.getElementById("encKey");
 const output = document.getElementById("output");
 const switchBtn = document.getElementById("switchAction");
+const copyBtn = document.getElementById("copyBtn");
 const IV = CryptoJS.enc.Hex.parse("101112131415161718191a1b1c1d1e1f");
 let encKeyVal;
 let isEnc = true;
 
-switchBtn.addEventListener('click', () => {
+switchBtn.addEventListener("click", () => {
   isEnc = !isEnc;
   if (isEnc) {
     switchBtn.innerText = "Have a cipher? Decrypt here!";
-    input1.value = '';
-    encKey.value = ''
-    encBtn.innerText = 'Encrypt'
-    output.innerHTML = ''
-    input1.placeholder = 'Enter text to encrypt here'
+    input1.value = "";
+    encKey.value = "";
+    encBtn.innerText = "Encrypt";
+    output.innerHTML = "";
+    input1.placeholder = "Enter text to encrypt here";
   } else {
     switchBtn.innerText = "Switch to Encryption";
-    input1.value = '';
-    encKey.value = ''
-    output.innerHTML = ''
-    encBtn.innerText = 'Decrypt'
-    input1.placeholder = 'Enter text to decrypt here'
+    input1.value = "";
+    encKey.value = "";
+    output.innerHTML = "";
+    encBtn.innerText = "Decrypt";
+    input1.placeholder = "Enter text to decrypt here";
   }
 });
 
 function encrypt(text, key, iv) {
   const options = { iv: iv };
   try {
-    const cipher = CryptoJS.AES.encrypt(text, key, options).toString();
-    return cipher;
+    if (text != "" && key != "") {
+      const cipher = CryptoJS.AES.encrypt(text, key, options).toString();
+      return cipher;
+    } else {
+      console.log("Empty string");
+    }
   } catch (error) {
     console.log("Encryption failed:", error.message);
   }
@@ -39,7 +44,9 @@ function encrypt(text, key, iv) {
 function decrypt(text, key, iv) {
   const options = { iv: iv };
   try {
-    const decipher = CryptoJS.AES.decrypt(text, key, options).toString(CryptoJS.enc.Utf8);
+    const decipher = CryptoJS.AES.decrypt(text, key, options).toString(
+      CryptoJS.enc.Utf8
+    );
     return decipher;
   } catch (error) {
     console.log("Decryption failed:", error.message);
@@ -51,7 +58,7 @@ encBtn.addEventListener("click", (e) => {
 
   let usrInp = input1.value;
   encKeyVal = encKey.value;
-  
+
   let result;
   if (isEnc) {
     result = encrypt(usrInp, encKeyVal, IV);
@@ -60,3 +67,21 @@ encBtn.addEventListener("click", (e) => {
   }
   output.innerHTML = result;
 });
+
+
+copyBtn.addEventListener('click', ()=>{
+  output.select();
+  try {
+    document.execCommand('copy')
+    Toastify({
+      text: "Copied",
+      duration: 2000,
+      position: "center"
+      }).showToast();      
+  } catch (error) {
+    Toastify({
+      text: error.message,
+      duration: 2000
+      }).showToast();
+  }
+})
